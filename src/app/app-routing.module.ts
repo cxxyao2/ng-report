43f50d62d7e1e0797b002559d65803aa023bf2b9
cxxyao2 <cxxyao2@gmail.com>
2021-08-30 00:16:24 -0400

    searchbox added to toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { GameCardComponent } from './animations/game-card/game-card.component';
 import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,7 +16,9 @@ const routes: Routes = [
   { path: 'product-list', component: ProductListComponent },
   { path: 'about-me', component: AboutComponent },
   { path: 'game-card', component: GameCardComponent },
+  { path: 'cart', component: CartComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+
   { path: '**', component: PageNotFoundComponent },
 ];
 
--- src/app/components/toolbar/search-box/search-box.component.ts
+++ src/app/components/toolbar/search-box/search-box.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
 import { SearchProductService } from 'src/app/services/search-product.service';
 @Component({
   selector: 'app-search-box',
@@ -7,20 +10,41 @@ import { SearchProductService } from 'src/app/services/search-product.service';
   styleUrls: ['./search-box.component.scss'],
 })
 export class SearchBoxComponent implements OnInit {
+  sub?: Subscription;
   constructor(
     private router: Router,
     private activeRoute: ActivatedRoute,
     private service: SearchProductService
   ) {}
 
-  ngOnInit(): void {}
+  productInput = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(50),
+    Validators.minLength(3),
+  ]);
 
-  setSearchTerms(term: string) {
-    this.service.setSearchTerms(term);
+  ngOnInit(): void {
+    this.sub = this.productInput.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap((data) => console.log(data)),
+        map((searchTerm: string) => {
+          this.service.setSearchTerms(searchTerm);
+          // if productList component is not active, navigate to it
+          if (!this.router.url.includes('product-list')) {
+            this.router.navigateByUrl('/product-list');
+          }
+        })
+      )
+      .subscribe(console.log);
+  }
 
-    // if productList component is not active, navigate to it
-    if (!this.router.url.includes('product-list')) {
-      this.router.navigateByUrl('/product-list');
-    }
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+
+  resetSearchTerm() {
+    this.productInput.reset();
   }
 }

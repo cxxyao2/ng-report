7f9a5c90787f9a75d26f350f62b733c8de78661f
cxxyao2 <cxxyao2@gmail.com>
2021-10-18 16:36:00 -0400

    modify key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: ' https://backend-cxx.herokuapp.com/api',
   imageUrl: 'https://backend-cxx.herokuapp.com/',
-  mapKey: 'AIzaSyBPo1tcYEp6UnJj4LuWFXEK6xJnnwGjQBw',
+  mapKey: 'w',
   weatherUrl: 'https://weather.',
   cookieName: 'xsrf-token',
   tokenHeaderName: 'x-xsrf-token',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: ' https://backend-cxx.herokuapp.com/api',
   imageUrl: 'https://backend-cxx.herokuapp.com/',
-  mapKey: 'AIzaSyBPo1tcYEp6UnJj4LuWFXEK6xJnnwGjQBw',
+  mapKey: 'A',
   weatherUrl: 'https://weather.',
   cookieName: 'xsrf-token',
   tokenHeaderName: 'x-xsrf-token',

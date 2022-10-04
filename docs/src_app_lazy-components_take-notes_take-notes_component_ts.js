6a709bd4793547e087798dfae08e697cb1570da8
cxxyao2 <cxxyao2@gmail.com>
2022-10-04 10:04:43 -0400

    rebuild

---
+++ docs/src_app_lazy-components_take-notes_take-notes_component_ts.js
@@ -0,0 +1,59 @@
+"use strict";
+(self["webpackChunkng_center"] = self["webpackChunkng_center"] || []).push([["src_app_lazy-components_take-notes_take-notes_component_ts"],{
+
+/***/ 22671:
+/*!********************************************************************!*\
+  !*** ./src/app/lazy-components/take-notes/take-notes.component.ts ***!
+  \********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "TakeNotesComponent": () => (/* binding */ TakeNotesComponent),
+/* harmony export */   "MyTakeNotesModule": () => (/* binding */ MyTakeNotesModule)
+/* harmony export */ });
+/* harmony import */ var _angular_material_dialog__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/material/dialog */ 22213);
+/* harmony import */ var _angular_material_form_field__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/material/form-field */ 65788);
+/* harmony import */ var _angular_material_input__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/material/input */ 64742);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ 2316);
+
+
+
+
+
+
+
+class TakeNotesComponent {
+    constructor() { }
+    ngOnInit() { }
+}
+TakeNotesComponent.ɵfac = function TakeNotesComponent_Factory(t) { return new (t || TakeNotesComponent)(); };
+TakeNotesComponent.ɵcmp = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: TakeNotesComponent, selectors: [["app-take-notes"]], decls: 10, vars: 0, consts: [["mat-dialog-title", ""], ["matInput", "", "placeholder", "Enter your first name"], ["matInput", "", "placeholder", "Enter your last name"], ["matInput", "", "placeholder", "Enter your location"], ["matInput", "", "placeholder", "Enter your age"]], template: function TakeNotesComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "h2", 0);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "My Form");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](2, "mat-form-field");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](3, "input", 1);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](4, "mat-form-field");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](5, "input", 2);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](6, "mat-form-field");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](7, "input", 3);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](8, "mat-form-field");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](9, "input", 4);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    } }, directives: [_angular_material_dialog__WEBPACK_IMPORTED_MODULE_1__.MatDialogTitle, _angular_material_form_field__WEBPACK_IMPORTED_MODULE_2__.MatFormField, _angular_material_input__WEBPACK_IMPORTED_MODULE_3__.MatInput], styles: ["mat-form-field[_ngcontent-%COMP%] {\n  margin: 16px;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRha2Utbm90ZXMuY29tcG9uZW50LnNjc3MiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFDRSxZQUFBO0FBQ0YiLCJmaWxlIjoidGFrZS1ub3Rlcy5jb21wb25lbnQuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbIm1hdC1mb3JtLWZpZWxkIHtcbiAgbWFyZ2luOiAxNnB4O1xufVxuIl19 */"] });
+class MyTakeNotesModule {
+}
+MyTakeNotesModule.ɵfac = function MyTakeNotesModule_Factory(t) { return new (t || MyTakeNotesModule)(); };
+MyTakeNotesModule.ɵmod = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: MyTakeNotesModule });
+MyTakeNotesModule.ɵinj = /*@__PURE__*/ _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ imports: [[_angular_material_form_field__WEBPACK_IMPORTED_MODULE_2__.MatFormFieldModule, _angular_material_input__WEBPACK_IMPORTED_MODULE_3__.MatInputModule, _angular_material_dialog__WEBPACK_IMPORTED_MODULE_1__.MatDialogModule]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](MyTakeNotesModule, { declarations: [TakeNotesComponent], imports: [_angular_material_form_field__WEBPACK_IMPORTED_MODULE_2__.MatFormFieldModule, _angular_material_input__WEBPACK_IMPORTED_MODULE_3__.MatInputModule, _angular_material_dialog__WEBPACK_IMPORTED_MODULE_1__.MatDialogModule] }); })();
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_app_lazy-components_take-notes_take-notes_component_ts.js.map

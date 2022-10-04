22b0da54e6ad68fbe9d6d56a644048458350fde1
cxxyao2 <cxxyao2@gmail.com>
2022-10-03 21:27:18 -0400

    modify server

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + "/dist/ng-center")); // serve our static files
+app.use(express.static(__dirname + "/docs")); // serve our static files
 
 // direct all of the requests to index.html
 app.all("/*", (req, res) => {
-  res.status(200).sendFile(__dirname + "/dist/ng-center/index.html");
+  res.status(200).sendFile(__dirname + "/docs/index.html");
 });
 app.listen(process.env.PORT || 8080);
 

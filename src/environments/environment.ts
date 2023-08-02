fa2255b4e14028a15372d51b36193cfafb384ceb
Jennifer Cheng <40253107+cxxyao2@users.noreply.github.com>
2023-08-02 10:26:49 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,3 @@
-export const environment = {
-  production: false,
-  apiUrl: ' https://bakend-jennifer.herokuapp.com/api',
-  imageUrl: 'https://bakend-jennifer.herokuapp.com',
-  mapKey: 'w',
-  weatherUrl: 'https://weather.',
-  cookieName: 'my-xsrf-token',
-  tokenHeaderName: 'my-x-xsrf-token',
-};
 
 // export const environment = {
 //   production: false,
